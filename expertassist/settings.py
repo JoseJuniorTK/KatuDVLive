806d144b75021ac726b74c2e6fb7c151e41ca404
JoseJuniorTK <69799917+JoseJuniorTK@users.noreply.github.com>
2021-11-25 21:35:08 -0300

    Update settings.py

--- expertassist/settings.py
+++ expertassist/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
